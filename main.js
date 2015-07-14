46591209e90e1484a73e02b6cfe9bd0c206c7575
Steve Domenico <stratcat944@gmail.com>
2015-07-14 09:49:19 -0600

    formatting fix

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 $(document).on('ready', function() {
 	$('.init-form').hide();
 	$('.form-two').hide();
-	
+
 $(".cat-add").on('click', function(event){
+	event.preventDefault()
 	$('.init-form').show();
 	});
 
 $(".next-add").on('click', function(event){
 	event.preventDefault()
 	$('.init-form').hide();
+	$('.cat-add').hide();
 	$('.form-two').show();
 	});
 
