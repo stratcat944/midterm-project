f30291aa95310e96a56df864537b6c655e9bf08a
Steve Domenico <stratcat944@gmail.com>
2015-07-14 10:35:48 -0600

    3rd form added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function() {
 	$('.init-form').hide();
 	$('.form-two').hide();
+	$('.form-three').hide();
 
 $(".cat-add").on('click', function(event){
 	event.preventDefault()
@@ -15,6 +16,12 @@ $(".next-add").on('click', function(event){
 	$('.form-two').show();
 	});
 
-
+$(".third-add").on('click', function(event){
+	event.preventDefault()
+	$('.init-form').hide();
+	$('.cat-add').hide();
+	$('.form-two').hide();
+	$('.form-three').show();
+})
   
 });  
