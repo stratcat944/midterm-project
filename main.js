265c0b82bcd0cd687bf3284d4223943e198dc0c3
Steve Domenico <stratcat944@gmail.com>
2015-07-19 14:53:20 -0600

    added logic to display profile page

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).on('ready', function() {
 	$('.form-three').hide();
 	$('.form-four').hide();
 	$('.form-five').hide();
+	$('#intakeDate').datepicker();
 
 $(".cat-add").on('click', function(event){
 	event.preventDefault()
@@ -13,72 +14,55 @@ $(".cat-add").on('click', function(event){
 
 $(".next-add").on('click', function(event){
 	event.preventDefault()
-	$('.form-one').hide();
-	$('.cat-add').hide();
-	$('.form-two').show();
-	});
-
-$(".third-add").on('click', function(event){
-	event.preventDefault()
-	$('.form-one').hide();
-	$('.cat-add').hide();
-	$('.form-two').hide();
-	$('.form-three').show();
-})
-
-$(".final-add").on('click', function(event){
-	event.preventDefault()
-	$('.form-one').hide();
-	$('.cat-add').hide();
-	$('.form-two').hide();
-	$('.form-three').hide();
-	$('.form-four').show();
-})
-
-$(".last-page").on('click', function(event){
-	event.preventDefault()
-	$('.form-one').hide();
-	$('.cat-add').hide();
-	$('.form-two').hide();
-	$('.form-three').hide();
-	$('.form-four').hide();
-	$('.form-five').show();
-
-})
-
-$('#intakeDate').datepicker();
-
-$('#submit1').on('click', function(){
 	var name = $('#name').val();
 	var age = $('#age').val();
 	var breed = $('#breed').val();
 	var coatColor = $('#coatColor').val();
 	var	eyeColor = $('#eyeColor').val();
 	var gender = $('#radio1 input:checked').val();
+	$('.form-one').hide();
+	$('.cat-add').hide();
+	$('.form-two').show();
+	$('.name').text(name);
+	});
 
-	console.log(name, age, breed, coatColor, eyeColor, gender);
-});
-
-$('#submit2').on('click', function(){
-	var textBox1 = $('#textArea1').val();
-
-	console.log(textBox1);
+$(".third-add").on('click', function(event){
+	event.preventDefault()
+	var healthNotes = $('#textArea1').val();
+	$('.form-one').hide();
+	$('.cat-add').hide();
+	$('.form-two').hide();
+	$('.form-three').show();
+	console.log(healthNotes);
 })
 
-$('#submit3').on('click', function(){
-	var textBox2 = $('#textArea2').val();
+$(".final-add").on('click', function(event){
+	event.preventDefault()
+	
+	var location = $('#textArea2').val();
 	var intakeDate = $('#intakeDate').val();
 	var placement = $('#radio2 input:checked').val();
-
-	console.log(textBox2, intakeDate, placement);
+	$('.form-one').hide();
+	$('.cat-add').hide();
+	$('.form-two').hide();
+	$('.form-three').hide();
+	$('.form-four').show();
+	console.log(location, intakeDate, placement);
 })
 
-$('#submit4').on('click', function(){
-	var textBox3 = $('#textArea3').val();
+$(".last-page").on('click', function(event){
+	event.preventDefault()
+	var history = $('#textArea3').val();
+	$('.form-one').hide();
+	$('.cat-add').hide();
+	$('.form-two').hide();
+	$('.form-three').hide();
+	$('.form-four').hide();
+	$('.form-five').show();
+	console.log(history);
 
-
-	console.log(textBox3);
 })
-  
+
+
 
 });  
